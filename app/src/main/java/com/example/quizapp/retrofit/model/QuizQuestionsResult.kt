68dda763c8e704ffec89217b76b8f164b615e4da
uladzislau <vlad.hilko@gmail.com>
2021-05-20 10:13:53 +0300

    fix: remove underscore from model

--- app/src/main/java/com/example/quizapp/retrofit/model/QuizQuestionsResult.kt
+++ app/src/main/java/com/example/quizapp/retrofit/model/QuizQuestionsResult.kt
@@ -1,6 +1,9 @@
 package com.example.quizapp.retrofit.model
 
+import com.google.gson.annotations.SerializedName
+
 data class QuizQuestionsResult(
-    val response_code: Int,
+    @SerializedName("response_code")
+    val responseCode: Int,
     val results: List<QuizQuestion>
 )
